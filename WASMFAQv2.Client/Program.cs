e17041ff084db0a8c7ebb2c9071ffb39c28ce2b0
jakef <jakefitzenreider@gmail.com>
2025-05-06 19:47:42 -0500

    Added Light/Dark Toggle

--- WASMFAQv2.Client/Program.cs
+++ WASMFAQv2.Client/Program.cs
@@ -2,6 +2,7 @@ using WASMFAQv2.Shared.Interfaces;
 using WASMFAQv2.Client.Services;
 using MudBlazor.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -9,5 +10,6 @@ builder.Services.AddMudServices();
 
 builder.Services.AddScoped<IFAQApiService, FAQApiService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
